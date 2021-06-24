fa5a4d6dbaf823254bd3753b9f252f14208724c3
= <=>
2021-06-24 09:58:46 +0100

    finally wrote a decent sliding window solution

--- sliding_window.py
+++ sliding_window.py
@@ -1,25 +1,45 @@
 # sliding window technique
 
-# sub arrays that add up to 9
-
-num_list = list(range(1,10))
-def up_to_nine(array):
-    pass
-
-print(num_list)
-
 # easy: given array of ints, find maxisum sum subarray of size
 
 eg_input = [-1,2,3,1,-3,2]
 
-# for this example, size of sub array is 2
-def max_sum_sub(array):
+def max_sum_sub(array, subarraysize):
+    
     max = 0
-    for i in range(len(array)-1):
-        test = array[i] + array[i+1]
+    for i in range(len(array) - (subarraysize -1)):
+        
+        test = sum(array[i: i+subarraysize])
         if test > max:
             max = test
     return max
-            
 
-print(max_sum_sub(eg_input))
+
+print(max_sum_sub(eg_input, 2), 'easy')
+
+            
+# Medium - dynamically sized sliding window - given an array of positive integers, find the subarrays of integers that add up to a given number
+
+def get_sub_arrays(input, desired):
+    
+    solutions = []
+    le_sum = 0
+    sum_start_index = 0
+
+    for item in range(len(input)):
+        le_sum += input[item]
+
+        while le_sum > desired:
+            le_sum -= input[sum_start_index]
+            sum_start_index += 1
+        
+        if le_sum == desired:
+            solutions.append(input[sum_start_index: item + 1])
+            
+    return solutions
+            
+exampleInput1 = [1, 7, 9, 4, 3, 2, 2]
+desiredSum1 = 7
+print(get_sub_arrays(exampleInput1, desiredSum1))
+        
+        
